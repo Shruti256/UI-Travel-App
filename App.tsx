8fc2c3669084f0108d59e81edefa8ef10b9e35e6
Shruti256 <73480836+Shruti256@users.noreply.github.com>
2023-04-25 20:02:07 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {FlatList,Text,View,StyleSheet,TouchableOpacity,Image, ScrollView} from 'react-native';
+import Module2 from './components/module2';
+import Module3 from './components/module3';
+import Module1 from './components/module1';
+import {TOP_PLACES} from './data/theme';
+import Module4 from './components/module4';
+
+const App = () => {
+  return (
+     <View>
+     <View>
+     <Module4 />
+     </View>
+     </View>
+     )
+}
+
+export default App;
